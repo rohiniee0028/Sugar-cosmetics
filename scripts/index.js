ecc509a10e5c0081e0beedd0cf5a51dbcf0346fa
rohini kushwaha <rohiniee0028@gmail.com>
2022-07-22 02:26:09 +0530

    pushed day3

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,3 @@
-import footer_container from "../Components/footer.js";
-document.querySelector('.footer_container').innerHTML=footer_container();
 
 document.querySelector(".fa-xmark").addEventListener("click", myFun);
 function myFun() {
@@ -28,3 +26,59 @@ const swiper = new Swiper('.swiper', {
 });
 
 // slideshow--end-------------------------------------------
+
+// slider after video starts--------------------
+
+let left = 1;
+let right = 4;
+function show(){
+    for(let i=left;i<=right;i++){
+        document.getElementById("c"+i).style.display="inline-block";
+    }
+}
+show();
+function moveLeft(){
+    if(left<=12 && right<=16){
+        document.getElementById("c"+left).style.display="none";
+        left+=1;
+        right+=1;
+        for(let i=left;i<=right;i++){
+         document.getElementById("c"+i).style.display="inline-block";
+        }
+    }else{
+        return;
+    }
+  
+}
+function moveRight(){
+    if(left>=1 && right>=5){
+        document.getElementById("c"+right).style.display="none";
+        left-=1;
+        right-=1;
+        for(let i=left;i<=right;i++){
+         document.getElementById("c"+i).style.display="inline-block";
+        }
+    }else{
+        return;
+    }
+ }
+// hot deals slider-------------------
+
+const productContainer = [...document.querySelectorAll('.product-container')];
+
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainer.forEach((item, i) =>{
+  let containerDimensions = item.getBoundingClientRect();
+  let containerWidth = containerDimensions.width;
+
+  nxtBtn[i].addEventListener('click', ()=>{
+     item.scrollLeft += containerWidth;
+  })
+  preBtn[i].addEventListener('click', ()=>{
+    item.scrollLeft -= containerWidth;
+ })
+ 
+});
+      
